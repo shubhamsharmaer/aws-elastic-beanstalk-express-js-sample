133e2e36b38e554b241f61e774a362381bbebf5f
Shubham Sharma <shubhamgetmail@gmail.com>
2024-07-26 17:36:56 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey Folks!, Shubham this side. Happy coding'));
+app.get('/', (req, res) => res.send('Testing Manual Review, If you see this, it means this is coming through Review stage in the Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
